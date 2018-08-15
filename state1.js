70564cd88a122a013b24d405884a5b726f2be159
maurizio <mauriziobonanno43@gmail.com>
2018-08-15 19:23:17 +0200

    aggiunto uno switch tra gli state

--- state1.js
+++ state1.js
@@ -5,6 +5,23 @@ demo.state1 = function(){
 
 demo.state1.prototype = {
     preload: function(){},
-    create: function(){},
+    create: function(){
+       game.stage.backgroundColor = '#DDDDDD';
+       addChangeStateListener();
+        
+    },
     update: function(){}
+};
+
+function changeState(i,num){
+    game.state.start('state'+num);
+}
+
+function addKeyCallback(key , fn , arg){
+    game.input.keyboard.addKey(key).onDown.add(fn,null,null,arg);
+}
+
+function addChangeStateListener(){
+    addKeyCallback(Phaser.Keyboard.TWO,changeState,2);
+    addKeyCallback(Phaser.Keyboard.ONE,changeState,1);
 }
