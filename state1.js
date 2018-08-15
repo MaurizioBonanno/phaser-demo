63b211684417f53ad89ed407c2be09202b88a89b
maurizio <mauriziobonanno43@gmail.com>
2018-08-15 22:32:48 +0200

    aggiunta una camera e confini del mondo

--- state1.js
+++ state1.js
@@ -2,7 +2,7 @@ var demo = {};
 var centerX =1500/2;
 var centerY = 1000/2;
 var player;
-var speed = 4;
+var speed = 2;
 demo.state1 = function(){
     
 };
@@ -10,12 +10,37 @@ demo.state1 = function(){
 demo.state1.prototype = {
     preload: function(){
         game.load.image('player','assets/sprites/player.png');
+        game.load.image('sfondo','assets/background/sfondo.jpg');
     },
     create: function(){
+        
+        //inizializzo la fisica
+       game.physics.startSystem(Phaser.Physics.ARCADE);
        game.stage.backgroundColor = '#DDDDDD';
        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+       var sfondo = game.add.sprite(0,0,'sfondo');
+        sfondo.scale.setTo(1.5,1.5);
+        
        player = game.add.sprite(centerX,centerY,'player');//aggiungo un'immagine allo state
        player.anchor.setTo(0.5,0.5);//il centro dell'immagine
+        
+        //aggiungo la fisica allo sprite
+        
+        game.physics.enable(player);
+        player.body.collideWorldBounds= true;//collide con i bordi del mondo
+        
+        //imposto i bounds
+        game.world.setBounds(0,0,1700,1500);
+        
+        //aggiungo la camera
+        
+       game.camera.follow(player);
+        
+        //ggiungo la deadzone
+        game.camera.deadzone= new Phaser.Rectangle(centerX - 300, 0,600,1000);
+       
+       
        addChangeStateListener();
         
     },
