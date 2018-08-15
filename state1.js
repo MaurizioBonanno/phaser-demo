37344d7ca39021c321bc4ff4164994cf60476dfd
maurizio <mauriziobonanno43@gmail.com>
2018-08-15 21:42:37 +0200

    aggiunto immagine e movimento di una immagine

--- state1.js
+++ state1.js
@@ -1,17 +1,38 @@
 var demo = {};
+var centerX =1500/2;
+var centerY = 1000/2;
+var player;
+var speed = 4;
 demo.state1 = function(){
     
 };
 
 demo.state1.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('player','assets/sprites/player.png');
+    },
     create: function(){
        game.stage.backgroundColor = '#DDDDDD';
        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+       player = game.add.sprite(centerX,centerY,'player');//aggiungo un'immagine allo state
+       player.anchor.setTo(0.5,0.5);//il centro dell'immagine
        addChangeStateListener();
         
     },
-    update: function(){}
+    update: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            player.x += speed;
+        };
+        if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            player.x -= speed;
+        };
+        if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            player.y -= speed;
+        };
+        if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            player.y += speed;
+        };
+    }
 };
 
 function changeState(i,num){
